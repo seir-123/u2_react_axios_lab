0c7304b23af1e280bf5c7d74478ebad1b14da14f
narniaeagle <ilgar2023@hotmail.com>
2023-02-22 09:07:33 -0500

    c

--- axios/src/components/StarshipsList.js
+++ axios/src/components/StarshipsList.js
@@ -6,13 +6,9 @@ const StarshipsList = () => {
     const [starShips, setStarShips] = useState()
     useEffect(() => {
         const fetchShips = async () => {
-            try {
               const response = await axios.get(`https://swapi.dev/api/starships`)
               console.log(response.data.results)
               setStarShips(response.data.results)
-            } catch (err) {
-                console.log(err)
-            }
           };
           fetchShips();
     }, [])
@@ -20,7 +16,7 @@ const StarshipsList = () => {
     return(
         <div>
             <h1>Ship Names</h1>
-            {starShips?.map((s) => ( //
+            {starShips?.map((s) => ( 
                  <p>{s.name}</p>
             ))}
         </div>
