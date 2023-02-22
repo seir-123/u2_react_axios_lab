c4a6735500f12b78acf57f88f489be7f11df7572
Duncan <duncanwoodpro@gmail.com>
2023-02-22 17:31:40 -0500

    committing now because I realize that I am going to have to redo a lot of my code from yesterday. It's good practice but I've got a mountain of work ahead of me.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
-import LandingPage from './components/LandingPage'
+import { Routes, Route } from "react-router-dom"
+import { useState } from "react"
 
+import FilmDetails from './components/FilmDetails'
 import FilmsList from './components/FilmsList'
-import StarshipList from './components/StarshipsList'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import PeopleList from './components/PeopleList'
+import PlanetsList from './components/PlanetsList'
+import SpeciesList from './components/SpeciesList'
+import StarshipsList from './components/StarshipsList'
+import StarshipDetails from './components/StarshipDetails'
+import VehiclesList from './components/VehiclesList';
 
 
 
@@ -10,7 +19,20 @@ function App() {
 
   return (
     <div className="App">
-      <LandingPage />
+      <Nav />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='FilmsList/:index' element={<FilmDetails films={films}/>}/>
+          <Route path='FilmsList' element={<FilmsList films={films}/>}/>
+          <Route path='PeopleList' element={<PeopleList people={people} />}/>
+          <Route path='PlanetsList' element={<PlanetsList planets={planets} />}/>
+          <Route path='SpeciesList' element={<SpeciesList species={species} />}/>
+          <Route path='StarshipsList' element={<StarshipsList starships={starships} />}/>
+          <Route path='StarshipsList/:index' element={<StarshipDetails starships={starships}/>}/>
+          <Route path='VehiclesList' element={<VehiclesList vehicles={vehicles} />}/>
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+export default function LandingPage() {
+  return (
+    <div className="home">
+      <div>
+        <img
+          style={{ display: "block", maxWidth: "100%" }}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png"
+          alt="logo"
+        />
+        <h1>Star Wars Forever</h1>
+        <h2>Find amazing details across the Star Wars universe!</h2>
+      </div>
+    </div>
+  );
+}
