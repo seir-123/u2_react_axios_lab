4fb0a7b37350968d16e470f2c95a6ef91c350788
Austin <austinih46@gmail.com>
2023-02-22 20:21:28 -0600

    updated sharshipsPage

--- star-wars-api/src/components/Main.js
+++ star-wars-api/src/components/Main.js
@@ -3,18 +3,23 @@ import Starships from "./Starships";
 import Home from "./Home";
 import Planets from "./Planets";
 import Characters from "./Characters";
+import StarshipsPage from "./StarshipsPage";
+import { useState, useEffect } from 'react'
+
 
 const Main = () => {
 
-
+    const [starships, setStarships] = useState ([])
 
     return (
         <div className="Main">
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/starships" element={<Starships/>}/>
+                <Route path="/starships" element={<Starships starships={starships} setStarships={setStarships}/>}/>
+                <Route path="/starships/:name" element={<StarshipsPage starships={starships} setStarships={setStarships}/>}/>
                 <Route path="/planets" element={<Planets/>}/>
                 <Route path="/characters" element={<Characters/>}/>
+                
             </Routes>
         </div>
     )
--- star-wars-api/src/components/Starships.js
+++ star-wars-api/src/components/Starships.js
@@ -1,20 +1,28 @@
-import StarshipsList from "./StarshipsList"
+import StarshipsPage from "./StarshipsPage"
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 import { STARSHIPS_URL } from "../globals";
+import { useNavigate } from "react-router-dom";
 
 
 
-const Starships = () => {
+const Starships = (props) => {
     
-    const [starships, setStarships] = useState ([])
     
+    
+    let navigate = useNavigate()
+
+    const showStarship = (starship) => {
+        navigate(`${starship.name}`)
+        console.log("did it work?")
+        console.log()
+    }
 
     useEffect(() =>{
         const getStarships = async () => {
             const response = await axios.get(`${STARSHIPS_URL}`)
             console.log(response.data.results)
-            setStarships(response.data.results)
+            props.setStarships(response.data.results)
         }
         getStarships()
     },[])
@@ -24,8 +32,8 @@ const Starships = () => {
             <h2 className="title">List of Starships</h2>
             <div className="listContainer">
             {
-                starships.map((starship) => (
-                    <div key={starship.name} className="item">
+                props.starships.map((starship) => (
+                    <div key={starship.name} className="item" onClick={() => showStarship(starship)} >
                         <h3>{starship.name}</h3>
                         
                     </div>
